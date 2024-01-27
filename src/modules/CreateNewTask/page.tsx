287e980f49037a06fa5101fbfcea2886d38960cd
Anna Yarossi <anna@cretia.dev>
2024-01-26 20:28:43 -0600

    Add form logic to CreateTask

--- src/modules/CreateNewTask/page.tsx
+++ src/modules/CreateNewTask/page.tsx
@@ -1,16 +1,58 @@
 "use client";
 import React from "react";
 import { Button, Heading, TextArea, TextField } from "@radix-ui/themes";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object().shape({
+  title: yup.string(),
+  description: yup.string(),
+});
 
 export default function CreateTask() {
+  const tasks = localStorage.getItem("tasks");
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      description: "",
+    },
+    validationSchema,
+    onSubmit(values) {
+      const tasksArr = [];
+
+      if (!tasks) {
+        tasksArr.push({ ...values });
+      } else {
+        const existingTasks = JSON.parse(tasks);
+        tasksArr.push(...existingTasks, { ...values });
+      }
+
+      const tasksStr = JSON.stringify(tasksArr);
+      localStorage.setItem("tasks", tasksStr);
+    },
+  });
+
   return (
     <div>
       <Heading as="h1">New task</Heading>
-      <TextField.Root>
-        <TextField.Input placeholder="Title"></TextField.Input>
-      </TextField.Root>
-      <TextArea placeholder="description" />
-      <Button>Create</Button>
+      <form id="task" onSubmit={formik.handleSubmit}>
+        <TextField.Root>
+          <TextField.Input
+            placeholder="Title"
+            id="title"
+            onChange={formik.handleChange}
+            value={formik.values.title}
+          ></TextField.Input>
+        </TextField.Root>
+        <TextArea
+          placeholder="description"
+          id="description"
+          onChange={formik.handleChange}
+          value={formik.values.description}
+        />
+      </form>
+      <Button form="task">Create</Button>
     </div>
   );
 }
